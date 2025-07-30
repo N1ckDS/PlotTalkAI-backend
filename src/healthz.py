927500661000eead67c1e4b855bb0a5a36eba66d
NickDS <dsnikhaev@gmail.com>
2025-07-31 00:50:44 +0700

    Update healthz.py

--- src/healthz.py
+++ src/healthz.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, Header, HTTPException, status
 router = APIRouter()
-@router.post("/healthz", status_code=status.HTTP_200_OK)
+@router.head("/healthz", status_code=status.HTTP_200_OK)
 def check_healthz():
     return {"status": "ok"}
