5e165290686b1d8137df2f1fedb6476ab206a101
NickDS <dsnikhaev@gmail.com>
2025-08-01 16:22:38 +0700

    Fix pool

--- db/database.py
+++ db/database.py
@@ -32,6 +32,7 @@ class DatabasePool:
         cls.max_conn = int(os.getenv('MAX_CONN'))
         cls.connect_pool()
     
+    @classmethod
     def connect_pool(cls):
         try:
             cls._pool = ThreadedConnectionPool(
